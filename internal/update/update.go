8b8cd62e6ffbf7aa8e7c89203904c402bb527d87
yplog <yalinpala@protonmail.com>
2025-06-14 22:29:57 +0300

    chore: remove command

--- internal/update/update.go
+++ internal/update/update.go
@@ -5,7 +5,6 @@ import (
 	"os/exec"
 )
 
-// Run downloads and installs the latest release of memotty.
 func Run() error {
 	cmd := exec.Command("bash", "-c", "curl -fsSL https://raw.githubusercontent.com/yplog/memotty/main/scripts/install.sh | bash")
 	cmd.Stdout = os.Stdout
